fd465791c91e900b7d59a9a76741e9fbb5c32292
Armterla <70601797@qq.com>
2017-02-27 13:48:24 +0800

    Step : HttpServer impl go a step futher.

--- include/gaia_network_httpserver.h
+++ include/gaia_network_httpserver.h
@@ -21,13 +21,67 @@ namespace GAIA
 {
 	namespace NETWORK
 	{
+		GAIA_ENUM_BEGIN(HTTP_SERVER_BLACKWHITE_MODE)
+			HTTP_SERVER_BLACKWHITE_MODE_BLACK, // Default value.
+			HTTP_SERVER_BLACKWHITE_MODE_WHITE,
+			HTTP_SERVER_BLACKWHITE_MODE_NONE,
+		GAIA_ENUM_END(HTTP_SERVER_BLACKWHITE_MODE)
+
 		class HttpServerDesc : public GAIA::Base
 		{
 		public:
-			GINL GAIA::GVOID reset(){}
-			GINL GAIA::BL check() const{return GAIA::True;}
+			static const GAIA::NUM DEFAULT_THREAD_COUNT = 4;
+			static const GAIA::NUM DEFAULT_MAX_CONN_COUNT = 10000;
+			static const GAIA::U64 DEFAULT_MAX_CONN_TIME = 1000 * 1000 * 3600 * 24;
+			static const GAIA::U64 DEFAULT_MAX_HALFCONN_TIME = 1000 * 1000 * 10;
+			static const GAIA::U64 DEFAULT_MAX_DYNAMIC_CACHE_SIZE = 1024 * 1024 * 1024;
+			static const GAIA::U64 DEFAULT_MAX_DYNAMIC_CACHE_COUNT = 1024 * 1024;
+			static const GAIA::U64 DEFAULT_MAX_STATIC_CACHE_SIZE = 1024 * 1024 * 10;
+			static const GAIA::U64 DEFAULT_MAX_STATIC_CACHE_COUNT = 1024 * 1024;
+			static const GAIA::U64 DEFAULT_MAX_RESPONSE_COUNT_PER_MINUTE = 100000;
 
 		public:
+			GINL GAIA::GVOID reset()
+			{
+				sThreadCount = DEFAULT_THREAD_COUNT;
+				pszRootPath = GNILSTR;
+				uMaxConnCount = DEFAULT_MAX_CONN_COUNT;
+				uMaxConnTime = DEFAULT_MAX_CONN_TIME;
+				uMaxHarfConnTime = DEFAULT_MAX_HALFCONN_TIME;
+				uMaxDynamicCacheSize = DEFAULT_MAX_DYNAMIC_CACHE_SIZE;
+				uMaxDynamicCacheCount = DEFAULT_MAX_DYNAMIC_CACHE_COUNT;
+				uMaxStaticCacheSize = DEFAULT_MAX_STATIC_CACHE_SIZE;
+				uMaxStaticCacheCount = DEFAULT_MAX_STATIC_CACHE_COUNT;
+				uMaxResponseCountPerMinute = DEFAULT_MAX_RESPONSE_COUNT_PER_MINUTE;
+			}
+			GINL GAIA::BL check() const
+			{
+				if(sThreadCount <= 0)
+					return GAIA::False;
+				if(pszRootPath == GNIL)
+					return GAIA::False;
+				if(uMaxConnCount == 0)
+					return GAIA::False;
+				if(uMaxConnTime == 0)
+					return GAIA::False;
+				if(uMaxHarfConnTime == 0)
+					return GAIA::False;
+				if(uMaxResponseCountPerMinute == 0)
+					return GAIA::False;
+				return GAIA::True;
+			}
+
+		public:
+			GAIA::NUM sThreadCount;
+			const GAIA::CH* pszRootPath;
+			GAIA::NUM uMaxConnCount;
+			GAIA::U64 uMaxConnTime;
+			GAIA::U64 uMaxHarfConnTime;
+			GAIA::U64 uMaxDynamicCacheSize;
+			GAIA::U64 uMaxDynamicCacheCount;
+			GAIA::U64 uMaxStaticCacheSize;
+			GAIA::U64 uMaxStaticCacheCount;
+			GAIA::U64 uMaxResponseCountPerMinute;
 		};
 
 		class HttpServerStatus : public GAIA::Base
@@ -83,77 +137,161 @@ namespace GAIA
 			friend class HttpServer;
 
 		public:
-			HttpServerLink(){this->init();}
+			HttpServerLink(GAIA::NETWORK::HttpServer& svr);
+			~HttpServerLink();
 
-			const HttpServerStatus& GetStatus() const;
+			GINL GAIA::NETWORK::HttpServer& GetServer() const{return *m_pSvr;}
+			GAIA::GVOID Response(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize, const GAIA::U64& uCacheTime = GINVALID);
+			GAIA::GVOID Close();
 
 			GINL GAIA::N32 compare(const HttpServerLink& src) const{}
 			GCLASS_COMPARE_BYCOMPARE(HttpServerLink)
 
 		private:
-			GINL GAIA::GVOID init(){}
+			GINL GAIA::GVOID init()
+			{
+				m_pSvr = GNIL;
+				m_pSock = GNIL;
+			}
 
 		private:
-			GAIA::NETWORK::AsyncSocket* pSock;
+			GAIA::NETWORK::HttpServer* m_pSvr;
+			GAIA::NETWORK::AsyncSocket* m_pSock;
 		};
 
-		class HttpServerCallBack : public GAIA::Base
+		class HttpServerCallBack : public GAIA::RefObject
 		{
 		public:
-			HttpServerCallBack();
-			~HttpServerCallBack();
+			HttpServerCallBack(HttpServer& svr);
+			virtual ~HttpServerCallBack();
 
-		public:
-			const HttpServerStatus& GetStatus() const;
+			GINL GAIA::NETWORK::HttpServer& GetServer() const{return *m_pSvr;}
 
-		protected:
-			virtual GAIA::BL OnRecv(HttpServerLink& l, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
-			virtual GAIA::BL OnPut(HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
-			virtual GAIA::BL OnPost(HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
-			virtual GAIA::BL OnGet(HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
-			virtual GAIA::BL OnHead(HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
-			virtual GAIA::BL OnDelete(HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
+			GINL const GAIA::NETWORK::HttpServerStatus& GetStatus() const{return m_status;}
 
 		protected:
-			HttpServerStatus& GetStatus();
+			virtual GAIA::BL OnRecv(GAIA::NETWORK::HttpServerLink& l, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
+			virtual GAIA::BL OnPut(GAIA::NETWORK::HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
+			virtual GAIA::BL OnPost(GAIA::NETWORK::HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize){return GAIA::False;}
+			virtual GAIA::BL OnGet(GAIA::NETWORK::HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
+			virtual GAIA::BL OnHead(GAIA::NETWORK::HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
+			virtual GAIA::BL OnDelete(GAIA::NETWORK::HttpServerLink& l, const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead){return GAIA::False;}
+
+		protected:
+			GINL HttpServerStatus& GetStatus(){return m_status;}
 
 		private:
-			GAIA::GVOID init();
+			GINL GAIA::GVOID init()
+			{
+				m_pSvr = GNIL;
+				m_status.reset();
+			}
 
 		private:
+			GAIA::NETWORK::HttpServer* m_pSvr;
+			GAIA::NETWORK::HttpServerStatus m_status;
 		};
 
 		class HttpServer : public GAIA::Base
 		{
+			friend class HttpLink;
+
 		public:
 			HttpServer();
 			~HttpServer();
 
-			GAIA::BL RegistCallBack(HttpServerCallBack& cb);
-			GAIA::BL UnregistCallBack(HttpServerCallBack& cb);
+			GAIA::BL RegistCallBack(GAIA::NETWORK::HttpServerCallBack& cb);
+			GAIA::BL UnregistCallBack(GAIA::NETWORK::HttpServerCallBack& cb);
 			GAIA::BL UnregistCallBackAll();
-			GAIA::BL IsRegistedCallBack(HttpServerCallBack& cb);
+			GAIA::BL IsRegistedCallBack(GAIA::NETWORK::HttpServerCallBack& cb);
 
-			GAIA::BL Create(const HttpServerDesc& desc);
+			GAIA::BL Create(const GAIA::NETWORK::HttpServerDesc& desc);
 			GAIA::BL Destroy();
-			GAIA::BL IsCreated() const;
-			const HttpServerDesc& GetDesc() const;
+			GAIA::BL IsCreated() const{return m_bCreated;}
+			const GAIA::NETWORK::HttpServerDesc& GetDesc() const{return m_desc;}
 
 			GAIA::BL Startup();
 			GAIA::BL Shutdown();
-			GAIA::BL IsStartuped() const;
+			GAIA::BL IsStartuped() const{return m_bStartuped;}
 
 			GAIA::BL OpenAddr(const GAIA::NETWORK::Addr& addr);
 			GAIA::BL CloseAddr(const GAIA::NETWORK::Addr& addr);
 			GAIA::BL CloseAddrAll();
-			GAIA::BL IsOpennedAddr() const;
+			GAIA::BL IsOpennedAddr(const GAIA::NETWORK::Addr& addr) const;
 			GAIA::NUM GetOpennedAddrCount() const;
 			const GAIA::NETWORK::Addr* GetOpennedAddr(GAIA::NUM sIndex) const;
 
-			const HttpServerStatus& GetStatus() const;
+			GAIA::GVOID EnableDynamicResponseCache(GAIA::BL bEnable);
+			GAIA::BL IsEnableDynamicResponseCache() const;
+			GAIA::GVOID EnableStaticResponseCache(GAIA::BL bEnable);
+			GAIA::BL IsEnableStaticResponseCache() const;
+
+			GAIA::GVOID SetBlackWhiteMode(GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE mode);
+			GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE GetBlackWhiteMode() const;
+			GAIA::GVOID AddBlackList(const GAIA::NETWORK::IP& ip, const GAIA::U64& uTime = GINVALID);
+			GAIA::GVOID RemoveBlackList(const GAIA::NETWORK::IP& ip);
+			GAIA::GVOID RemoveBlackListAll();
+			GAIA::GVOID AddWhiteList(const GAIA::NETWORK::IP& ip, const GAIA::U64& uTime = GINVALID);
+			GAIA::GVOID RemoveWhiteList(const GAIA::NETWORK::IP& ip);
+			GAIA::GVOID RemoveWhiteListAll();
+
+			GINL const GAIA::NETWORK::HttpServerStatus& GetStatus() const{return m_status;}
 
 		private:
+			GAIA::GVOID AddDynamicCache(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize);
+			GAIA::GVOID RemoveDynamicCache(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead);
+			GAIA::GVOID RemoveDynamicCacheAll();
+
+			GAIA::GVOID RequestStaticCache(const GAIA::NETWORK::HttpURL& url);
+			GAIA::GVOID RemoveStaticCache(const GAIA::NETWORK::HttpURL& url);
+			GAIA::GVOID RemoveStaticCacheAll();
+
+		private:
+			GINL GAIA::GVOID init()
+			{
+				m_desc.reset();
+				m_bCreated = GAIA::False;
+				m_bStartuped = GAIA::False;
+				m_bEnableDynamicResponseCache = GAIA::True;
+				m_bEnableStaticResponseCache = GAIA::True;
+				m_blackwhitemode = GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE_BLACK;
+				m_status.reset();
+			}
+
+		private:
+			class WBNode : public GAIA::Base // White and black node.
+			{
+			public:
+				GINL GAIA::N32 compare(const WBNode& src) const{return ip.compare(src.ip);}
+				GCLASS_COMPARE_BYCOMPARE(WBNode)
+
+			public:
+				GAIA::NETWORK::IP ip;
+				GAIA::U64 uRegistTime; // Real time in microseconds.
+				GAIA::U64 uEffectTime; // Relative time in microseconds.
+			};
+
+		private:
+			GAIA::NETWORK::HttpServerDesc m_desc;
+			GAIA::SYNC::LockRW m_rwCBS;
+			GAIA::CTN::Vector<GAIA::NETWORK::HttpServerCallBack*> m_cbs;
+			GAIA::BL m_bCreated;
+			GAIA::BL m_bStartuped;
+
+			GAIA::BL m_bEnableDynamicResponseCache;
+			GAIA::BL m_bEnableStaticResponseCache;
+
+			GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE m_blackwhitemode;
+			GAIA::SYNC::LockRW m_rwBlackList;
+			GAIA::CTN::Set<WBNode> m_BlackList;
+			GAIA::SYNC::LockRW m_rwWhiteList;
+			GAIA::CTN::Set<WBNode> m_WhiteList;
+
+			GAIA::SYNC::LockRW m_rwDynamicCache;
+			GAIA::SYNC::LockRW m_rwStaticCache;
+
 			GAIA::NETWORK::AsyncDispatcher m_disp;
+			GAIA::NETWORK::HttpServerStatus m_status;
 		};
 	}
 }
--- src/gaia_network_httpserver.cpp
+++ src/gaia_network_httpserver.cpp
@@ -8,5 +8,273 @@ namespace GAIA
 {
 	namespace NETWORK
 	{
+		HttpServerLink::HttpServerLink(GAIA::NETWORK::HttpServer& svr)
+		{
+			this->init();
+		}
+
+		HttpServerLink::~HttpServerLink()
+		{
+
+		}
+
+		GAIA::GVOID HttpServerLink::Response(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize, const GAIA::U64& uCacheTime)
+		{
+
+		}
+
+		GAIA::GVOID HttpServerLink::Close()
+		{
+
+		}
+
+		HttpServerCallBack::HttpServerCallBack(HttpServer& svr)
+		{
+			this->init();
+		}
+
+		HttpServerCallBack::~HttpServerCallBack()
+		{
+
+		}
+
+		HttpServer::HttpServer()
+		{
+			this->init();
+		}
+
+		HttpServer::~HttpServer()
+		{
+			if(this->IsCreated())
+				this->Destroy();
+		}
+
+		GAIA::BL HttpServer::RegistCallBack(GAIA::NETWORK::HttpServerCallBack& cb)
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GAIA::SYNC::AutolockW al(m_rwCBS);
+			if(this->IsRegistedCallBack(cb))
+				return GAIA::False;
+			m_cbs.push_back(&cb);
+			cb.rise_ref();
+			return GAIA::True;
+		}
+
+		GAIA::BL HttpServer::UnregistCallBack(GAIA::NETWORK::HttpServerCallBack& cb)
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GAIA::SYNC::AutolockW al(m_rwCBS);
+			for(GAIA::NUM x = 0; x < m_cbs.size(); ++x)
+			{
+				if(m_cbs[x] == &cb)
+				{
+					m_cbs.erase(x);
+					cb.drop_ref();
+					return GAIA::True;
+				}
+			}
+			return GAIA::False;
+		}
+
+		GAIA::BL HttpServer::UnregistCallBackAll()
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GAIA::SYNC::AutolockW al(m_rwCBS);
+			for(GAIA::NUM x = 0; x < m_cbs.size(); ++x)
+			{
+				GAIA::NETWORK::HttpServerCallBack* pCB = m_cbs[x];
+				pCB->drop_ref();
+			}
+			m_cbs.clear();
+		}
+
+		GAIA::BL HttpServer::IsRegistedCallBack(GAIA::NETWORK::HttpServerCallBack& cb)
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GAIA::SYNC::AutolockR al(m_rwCBS);
+			for(GAIA::NUM x = 0; x < m_cbs.size(); ++x)
+			{
+				if(&cb == m_cbs[x])
+					return GAIA::True;
+			}
+			return GAIA::False;
+		}
+
+		GAIA::BL HttpServer::Create(const GAIA::NETWORK::HttpServerDesc& desc)
+		{
+			if(this->IsCreated())
+				return GAIA::False;
+
+			// Create async dispatcher.
+
+			m_bCreated = GAIA::True;
+			return GAIA::True;
+		}
+
+		GAIA::BL HttpServer::Destroy()
+		{
+			if(!this->IsCreated())
+				return GAIA::False;
+
+			//
+			if(this->IsStartuped())
+				this->Shutdown();
+
+			// Destroy async dispatcher.
+
+			//
+			this->UnregistCallBackAll();
+
+			m_bCreated = GAIA::False;
+			return GAIA::True;
+		}
+
+		GAIA::BL HttpServer::Startup()
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			if(this->IsStartuped())
+				return GAIA::False;
+
+			// Startup async dispatcher.
+
+			m_bStartuped = GAIA::True;
+			return GAIA::True;
+		}
+
+		GAIA::BL HttpServer::Shutdown()
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			if(!this->IsStartuped())
+				return GAIA::False;
+
+			// Shutdown async dispatcher.
+
+			m_bStartuped = GAIA::False;
+			return GAIA::True;
+		}
+
+		GAIA::BL HttpServer::OpenAddr(const GAIA::NETWORK::Addr& addr)
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GPCHR_FALSE_RET(addr.check(), GAIA::False);
+
+			//
+		}
+
+		GAIA::BL HttpServer::CloseAddr(const GAIA::NETWORK::Addr& addr)
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+			GPCHR_FALSE_RET(addr.check(), GAIA::False);
+		}
+
+		GAIA::BL HttpServer::CloseAddrAll()
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+		}
+
+		GAIA::BL HttpServer::IsOpennedAddr(const GAIA::NETWORK::Addr& addr) const
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GAIA::False);
+		}
+
+		GAIA::NUM HttpServer::GetOpennedAddrCount() const
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), 0);
+		}
+
+		const GAIA::NETWORK::Addr* HttpServer::GetOpennedAddr(GAIA::NUM sIndex) const
+		{
+			GPCHR_FALSE_RET(this->IsCreated(), GNIL);
+		}
+
+		GAIA::GVOID HttpServer::EnableDynamicResponseCache(GAIA::BL bEnable)
+		{
+			m_bEnableDynamicResponseCache = bEnable;
+		}
+
+		GAIA::BL HttpServer::IsEnableDynamicResponseCache() const
+		{
+			return m_bEnableDynamicResponseCache;
+		}
+
+		GAIA::GVOID HttpServer::EnableStaticResponseCache(GAIA::BL bEnable)
+		{
+			m_bEnableStaticResponseCache = bEnable;
+		}
+
+		GAIA::BL HttpServer::IsEnableStaticResponseCache() const
+		{
+			return m_bEnableStaticResponseCache;
+		}
+
+		GAIA::GVOID HttpServer::SetBlackWhiteMode(GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE mode)
+		{
+			m_blackwhitemode = mode;
+		}
+
+		GAIA::NETWORK::HTTP_SERVER_BLACKWHITE_MODE HttpServer::GetBlackWhiteMode() const
+		{
+			return m_blackwhitemode;
+		}
+
+		GAIA::GVOID HttpServer::AddBlackList(const GAIA::NETWORK::IP& ip, const GAIA::U64& uTime)
+		{
+			GAIA::SYNC::AutolockW al(m_rwBlackList);
+		}
+
+		GAIA::GVOID HttpServer::RemoveBlackList(const GAIA::NETWORK::IP& ip)
+		{
+			GAIA::SYNC::AutolockW al(m_rwBlackList);
+		}
+
+		GAIA::GVOID HttpServer::RemoveBlackListAll()
+		{
+			GAIA::SYNC::AutolockW al(m_rwBlackList);
+		}
+
+		GAIA::GVOID HttpServer::AddWhiteList(const GAIA::NETWORK::IP& ip, const GAIA::U64& uTime)
+		{
+			GAIA::SYNC::AutolockW al(m_rwWhiteList);
+		}
+
+		GAIA::GVOID HttpServer::RemoveWhiteList(const GAIA::NETWORK::IP& ip)
+		{
+			GAIA::SYNC::AutolockW al(m_rwWhiteList);
+		}
+
+		GAIA::GVOID HttpServer::RemoveWhiteListAll()
+		{
+			GAIA::SYNC::AutolockW al(m_rwWhiteList);
+		}
+
+		GAIA::GVOID HttpServer::AddDynamicCache(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead, const GAIA::GVOID* p, GAIA::NUM sSize)
+		{
+			GAIA::SYNC::AutolockW al(m_rwDynamicCache);
+		}
+
+		GAIA::GVOID HttpServer::RemoveDynamicCache(const GAIA::NETWORK::HttpURL& url, const GAIA::NETWORK::HttpHead& httphead)
+		{
+			GAIA::SYNC::AutolockW al(m_rwDynamicCache);
+		}
+
+		GAIA::GVOID HttpServer::RemoveDynamicCacheAll()
+		{
+			GAIA::SYNC::AutolockW al(m_rwDynamicCache);
+		}
+
+		GAIA::GVOID HttpServer::RequestStaticCache(const GAIA::NETWORK::HttpURL& url)
+		{
+			GAIA::SYNC::AutolockW al(m_rwStaticCache);
+		}
+
+		GAIA::GVOID HttpServer::RemoveStaticCache(const GAIA::NETWORK::HttpURL& url)
+		{
+			GAIA::SYNC::AutolockW al(m_rwStaticCache);
+		}
+
+		GAIA::GVOID HttpServer::RemoveStaticCacheAll()
+		{
+			GAIA::SYNC::AutolockW al(m_rwStaticCache);
+		}
 	}
 }
