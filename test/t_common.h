b4c290d764bfa863c0c82fdd3cdd623fc7e1e7b8
Armterla <70601797@qq.com>
2017-03-12 02:14:44 +0800

    Test case : Support TLOG macro in test case.

--- test/t_common.h
+++ test/t_common.h
@@ -1,12 +1,13 @@
 #ifndef		__T_COMMON_H__
 #define		__T_COMMON_H__
 
-#define TTEXT(sz) do{logobj << sz << logobj.End();}while(0)
+#define TTEXT(sz) do{logobj << logobj.Type(GAIA::LOG::Log::TYPE_LOG) << sz << logobj.End();}while(0)
 #define TITEM(sz) do{if(!bOutputTime)\
 							uTimeBegin = GAIA::TIME::tick_time();\
 						else\
 						{\
 							uTimeEnd = GAIA::TIME::tick_time();\
+							logobj << logobj.Type(GAIA::LOG::Log::TYPE_LOG);\
 							logobj << "\t\tTimelost ";\
 							logobj << GSCAST(GAIA::F64)(uTimeEnd - uTimeBegin) / 1000.0;\
 							logobj << "(MS)";\
@@ -16,6 +17,7 @@
 						logobj << "\t" << (sz) << logobj.End(); \
 					}while(0)
 #define TERROR do{logobj << logobj.Type(GAIA::LOG::Log::TYPE_ERROR) << "\t\t" << __FILE__ << "(" << __LINE__ << ")" << "\n" << logobj.End();}while(0)
+#define TLOG(t) do{logobj << logobj.Type(GAIA::LOG::Log::TYPE_LOG) << "\t\t" << (t) << "\n" << logobj.End();}while(0)
 #define TAST(e) do{if(!(e)) TERROR;}while(0)
 
 #endif
