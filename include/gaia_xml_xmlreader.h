5a8a5eb0ca98bb82574ac15a33110af7c20d8182
Armterla <70601797@qq.com>
2017-03-15 14:46:08 +0800

    Step : XmlReader impl go a step further.

--- include/gaia_xml_xmlreader.h
+++ include/gaia_xml_xmlreader.h
@@ -111,10 +111,6 @@ namespace GAIA
 
 				@param pNext [out] Used for saving the next read location(read pointer)
 
-				@param pValueIsString [out] Used for saving the value node's string type.
-					If value is a string value, it will be filled by GAIA::True, or will be filled by GAIA::False.\n
-					If the next node is not a value node, this parameter is be ignored.
-
 				@return Return the node's name with out '\0'.
 
 				@exception GAIA::ECT::EctIllegal
@@ -125,7 +121,172 @@ namespace GAIA
 			*/
 			GINL const _DataType* Peek(GAIA::XML::XML_NODE& nt, _SizeType& nodenamelen, _DataTypePtr* pNext = GNIL)
 			{
+				m_pCursor = this->move_to_next(m_pCursor);
+				if(m_pCursor == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				const _DataType* pLocalNext = GNIL;
+				const _DataType* p = m_pCursor;
+				const _DataType* pRet = GNIL;
+				switch(*p)
+				{
+				case '<': // Container begin or multi container begin or multi container end.
+					{
+						if(p == m_pBack)
+							GTHROW_RET(DataError, GNIL);
+						++p;
+						if(*p == '/') // Multi container end.
+						{
+							++p;
+							if(!GAIA::ALGO::isalpha(*p))
+								GTHROW_RET(DataError, GNIL);
+							while(p <= m_pBack)
+							{
+								if(!GAIA::ALGO::isalpha(*p) &&
+								   !GAIA::ALGO::isdigit(*p) &&
+								   *p != '_' &&
+								   *p != '_')
+									break;
+								++p;
+							}
+							if(p > m_pBack)
+								GTHROW_RET(DataError, GNIL);
+							nt = GAIA::XML::XML_NODE_MULTICONTAINER;
+							nodenamelen = (_SizeType)(p - m_pCursor - 2);
+							pRet = m_pCursor + 2;
 
+							p = this->move_to_next(p);
+							if(*p != '>')
+								GTHROW_RET(DataError, GNIL);
+							pLocalNext = p + 1;
+						}
+						else // Container begin or multi container begin.
+						{
+							if(!GAIA::ALGO::isalpha(*p))
+								GTHROW_RET(DataError, GNIL);
+							while(p <= m_pBack)
+							{
+								if(!GAIA::ALGO::isalpha(*p) &&
+								   !GAIA::ALGO::isdigit(*p) &&
+								   *p != '_' &&
+								   *p != '_')
+									break;
+								++p;
+							}
+							if(p > m_pBack)
+								GTHROW_RET(DataError, GNIL);
+							nt = GAIA::XML::XML_NODE_CONTAINER;
+							nodenamelen = (_SizeType)(p - m_pCursor - 1);
+							pLocalNext = p;
+							pRet = m_pCursor + 1;
+
+							// THIS CODE WILL COST MORE PERFORMANCE, IF NOT NEED, COULD CANCEL IT.
+							// Check is multi container.
+							{
+								GAIA::BL bInQuote = GAIA::False;
+								GAIA::BL bCheckMultiContainerComplete = GAIA::False;
+								while(p <= m_pBack)
+								{
+									switch(*p)
+									{
+									case '/':
+										{
+											if(!bInQuote)
+											{
+												nt = GAIA::XML::XML_NODE_CONTAINER;
+												bCheckMultiContainerComplete = GAIA::True;
+											}
+										}
+										break;
+									case '>':
+										{
+											if(!bInQuote)
+											{
+												nt = GAIA::XML::XML_NODE_MULTICONTAINER;
+												bCheckMultiContainerComplete = GAIA::True;
+											}
+										}
+										break;
+									case '\"':
+										bInQuote = !bInQuote;
+										break;
+									default:
+										break;
+									}
+									if(bCheckMultiContainerComplete)
+										break;
+									++p;
+								}
+								if(p > m_pBack)
+									GTHROW_RET(DataError, GNIL);
+							}
+							// END.
+						}
+					}
+					break;
+				case '>': // Multi container body begin.
+					{
+						m_pCursor = this->move_to_next(m_pCursor + 1);
+						pRet = this->Peek(nt, nodenamelen, &pLocalNext);
+					}
+					break;
+				case '/': // Container end.
+					{
+						if(p == m_pBack)
+							GTHROW_RET(DataError, GNIL);
+						if(*(p + 1) != '>')
+							GTHROW_RET(DataError, GNIL);
+						nt = GAIA::XML::XML_NODE_CONTAINER;
+						nodenamelen = 0;
+						pLocalNext = p + 2;
+						pRet = p;
+					}
+					break;
+				case '=': // Value.
+					{
+						pLocalNext = p = this->move_to_next(p + 1);
+						if(pLocalNext == GNIL)
+							GTHROW_RET(DataError, GNIL);
+						if(*pLocalNext != '\"')
+							GTHROW_RET(DataError, GNIL);
+						while(pLocalNext <= m_pBack)
+						{
+							if(*pLocalNext == '\"' && pLocalNext > p && pLocalNext[-1] != '\\')
+								break;
+							++pLocalNext;
+						}
+						if(pLocalNext > m_pBack)
+							GTHROW_RET(DataError, GNIL);
+						nt = GAIA::XML::XML_NODE_VALUE;
+						nodenamelen = (_SizeType)(pLocalNext - p - 1);
+						pLocalNext = pLocalNext + 1;
+						pRet = p + 1;
+					}
+					break;
+				default: // Name.
+					{
+						if(!GAIA::ALGO::isalpha(*p))
+							GTHROW_RET(DataError, GNIL);
+						while(p <= m_pBack)
+						{
+							if(!GAIA::ALGO::isalpha(*p) &&
+							   !GAIA::ALGO::isdigit(*p) &&
+							   *p != '_' &&
+							   *p != '_')
+								break;
+							++p;
+						}
+						if(p > m_pBack)
+							GTHROW_RET(DataError, GNIL);
+						nt = GAIA::XML::XML_NODE_NAME;
+						nodenamelen = (_SizeType)(p - m_pCursor);
+						pLocalNext = p;
+						pRet = m_pCursor;
+					}
+					break;
+				}
+				if(pNext != GNIL)
+					*pNext = pLocalNext;
+				return pRet;
 			}
 
 			/*!
@@ -147,7 +308,21 @@ namespace GAIA
 			*/
 			GINL const _DataType* Begin(GAIA::XML::XML_NODE& nt, _SizeType& nodenamelen)
 			{
-
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_CONTAINER:
+					break;
+				case GAIA::XML::XML_NODE_MULTICONTAINER:
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				m_pCursor = pNext;
+				return pRet;
 			}
 
 			/*!
@@ -164,11 +339,27 @@ namespace GAIA
 
 				@exception GAIA::ECT::EctDataError
 					If the source xml's format exist error, and can't read, throw it.
-
 			*/
-			GINL GAIA::GVOID End()
+			GINL const _DataType* End()
 			{
-
+				GAIA::XML::XML_NODE nt;
+				_SizeType nodenamelen;
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_CONTAINER:
+					GAST(nodenamelen == 0);
+					break;
+				case GAIA::XML::XML_NODE_MULTICONTAINER:
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				m_pCursor = pNext;
+				return pRet;
 			}
 
 			/*!
@@ -188,14 +379,392 @@ namespace GAIA
 			*/
 			GINL const _DataType* Read(GAIA::XML::XML_NODE& nt, _SizeType& nodenamelen)
 			{
-
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_NAME:
+					break;
+				case GAIA::XML::XML_NODE_VALUE:
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				GAST(nodenamelen != 0);
+				m_pCursor = pNext;
+				return pRet;
 			}
+
+			/*!
+				@brief Read a name node's name.
+
+				@param p [out] Used for saving name node's name.
+
+				@param maxlen [in] Specify the parameter p's buffer size in characters.
+
+				@return If success return parameter p.
+
+				@exception GAIA::ECT::EctInvalidParam
+					If parameter p is GNIL or (maxlen != GINVALID and maxlen below equal zero), throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a name node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a name node, throw it.
+
+				@exception GAIA::ECT::EctBufNotEnough
+					If parameter maxlen is not GINVALID and buffer is not enough for save the name node's name, throw it.
+			*/
+			template<typename _ParamDataType> _ParamDataType* ReadName(_ParamDataType* p, _SizeType maxlen = GINVALID)
+			{
+				if(p == GNIL)
+					GTHROW_RET(InvalidParam, GNIL);
+				if(maxlen != GINVALID && maxlen <= 0)
+					GTHROW_RET(InvalidParam, GNIL);
+				GAIA::XML::XML_NODE nt;
+				_SizeType nodenamelen;
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_NAME:
+					{
+						if(maxlen != GINVALID && nodenamelen >= maxlen)
+							GTHROW_RET(BufNotEnough, GNIL);
+						GAIA::ALGO::gstrcpy(p, pRet, nodenamelen);
+					}
+					break;
+				case GAIA::XML::XML_NODE_VALUE:
+					GTHROW_RET(Illegal, GNIL);
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				GAST(nodenamelen != 0);
+				m_pCursor = pNext;
+				return p;
+			}
+
+			/*!
+				@brief Read a value node's name.
+
+				@param p [out] Used for saving value node's name.
+
+				@param maxlen [in] Specify the parameter p's buffer size in characters.
+
+				@return If success return parameter p.
+
+				@exception GAIA::ECT::EctInvalidParam
+					If parameter p is GNIL or (maxlen != GINVALID and maxlen below equal zero), throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctBufNotEnough
+					If parameter maxlen is not GINVALID and buffer is not enough for save the value node's name, throw it.
+			*/
+			template<typename _ParamDataType> _ParamDataType* ReadValue(_ParamDataType* p, _SizeType maxlen = GINVALID)
+			{
+				if(p == GNIL)
+					GTHROW_RET(InvalidParam, GNIL);
+				if(maxlen != GINVALID && maxlen <= 0)
+					GTHROW_RET(InvalidParam, GNIL);
+				GAIA::XML::XML_NODE nt;
+				_SizeType nodenamelen;
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_NAME:
+					GTHROW_RET(Illegal, GNIL);
+				case GAIA::XML::XML_NODE_VALUE:
+					{
+						if(maxlen != GINVALID && nodenamelen >= maxlen)
+							GTHROW_RET(BufNotEnough, GNIL);
+						GAIA::ALGO::gstrcpy(p, pRet, nodenamelen);
+					}
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				GAST(nodenamelen != 0);
+				m_pCursor = pNext;
+				return p;
+			}
+
+			/*!
+				@brief Read GAIA::BL value node.
+
+				@param v [out] Used for saving the GAIA::BL value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::BL& ReadValue(GAIA::BL& v)
+			{
+				GAIA::XML::XML_NODE nt;
+				_SizeType nodenamelen;
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_NAME:
+					GTHROW_RET(Illegal, GNIL);
+				case GAIA::XML::XML_NODE_VALUE:
+					{
+						if(GAIA::ALGO::gstrcmp(pRet, "true", nodenamelen) == 0)
+							v = GAIA::True;
+						else if(GAIA::ALGO::gstrcmp(pRet, "false", nodenamelen) == 0)
+							v = GAIA::False;
+						else
+							GTHROW_RET(Convert, v);
+					}
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				GAST(nodenamelen != 0);
+				m_pCursor = pNext;
+				return v;
+			}
+
+			/*!
+				@brief Read a GAIA::X128 value node.
+
+				@param v [out] Used for saving the GAIA::X128 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::X128& ReadValue(GAIA::X128& v)
+			{
+				GAIA::XML::XML_NODE nt;
+				_SizeType nodenamelen;
+				const _DataType* pNext;
+				const _DataType* pRet = this->Peek(nt, nodenamelen, &pNext);
+				if(pRet == GNIL)
+					GTHROW_RET(Illegal, GNIL);
+				switch(nt)
+				{
+				case GAIA::XML::XML_NODE_NAME:
+					GTHROW_RET(Illegal, GNIL);
+				case GAIA::XML::XML_NODE_VALUE:
+					{
+						if(nodenamelen != 32)
+							GTHROW_RET(Convert, GNIL);
+						GAIA::CH sz[33];
+						GAIA::ALGO::gstrcpy(sz, pRet, nodenamelen);
+						if(!v.check(sz))
+							GTHROW_RET(Convert, GNIL);
+						v.fromstring(sz);
+					}
+					break;
+				default:
+					GTHROW_RET(Illegal, GNIL);
+				}
+				GAST(nodenamelen != 0);
+				m_pCursor = pNext;
+				return v;
+			}
+
+			/*!
+				@brief Read a GAIA::U8 value node.
+
+				@param v [out] Used for saving the GAIA::U8 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::U8& ReadValue(GAIA::U8& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::N8 value node.
+
+				@param v [out] Used for saving the GAIA::N8 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::N8& ReadValue(GAIA::N8& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::U16 value node.
+
+				@param v [out] Used for saving the GAIA::U16 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::U16& ReadValue(GAIA::U16& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::N16 value node.
+
+				@param v [out] Used for saving the GAIA::N16 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::N16& ReadValue(GAIA::N16& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::U32 value node.
+
+				@param v [out] Used for saving the GAIA::U32 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::U32& ReadValue(GAIA::U32& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::N32 value node.
+
+				@param v [out] Used for saving the GAIA::N32 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::N32& ReadValue(GAIA::N32& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::U64 value node.
+
+				@param v [out] Used for saving the GAIA::U64 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::U64& ReadValue(GAIA::U64& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::N64 value node.
+
+				@param v [out] Used for saving the GAIA::N64 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::N64& ReadValue(GAIA::N64& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::F32 value node.
+
+				@param v [out] Used for saving the GAIA::F32 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::F32& ReadValue(GAIA::F32& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
+
+			/*!
+				@brief Read a GAIA::U64 value node.
+
+				@param v [out] Used for saving the GAIA::F64 value.
+
+				@exception GAIA::ECT::EctIllegal
+					If can't read a value node's name, throw it.
+
+				@exception GAIA::ECT::EctIllegal
+					If the node is not a value node, throw it.
+
+				@exception GAIA::ECT::EctConvert
+					If the string can't convert to type of parameter v, throw it.
+			*/
+			GINL GAIA::F64& ReadValue(GAIA::F64& v){GAIA::CH szValue[64]; this->ReadValue(szValue, sizeof(szValue)); v = GAIA::ALGO::acasts(szValue); return v;}
 
 		private:
 			GINL GAIA::GVOID init()
 			{
 				m_pFront = m_pBack = m_pCursor = GNIL;
 				m_size = 0;
+			}
+			GINL const _DataType* move_to_next(const _DataType* p)
+			{
+				while(p <= m_pBack)
+				{
+					if(*p != ' ' &&
+					   *p != '\t' &&
+					   *p != '\r' &&
+					   *p != '\n')
+						return p;
+					++p;
+				}
+				GTHROW_RET(DataError, GNIL);
 			}
 
 		private:
