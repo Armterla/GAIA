1a5e3e07e7c5121868fcc4dcc4b73e0ec0fb1dfe
Armterla <70601797@qq.com>
2017-05-11 12:27:24 +0800

    Performance optimize : CRC algorithm performance optimize. Test case : Add test case for CRC. Code optimize : CRC code optimize.

--- include/gaia_digit_crc.h
+++ include/gaia_digit_crc.h
@@ -9,103 +9,7 @@ namespace GAIA
 {
 	namespace DIGIT
 	{
-		/*!
-		 	@brief Stream based CRC8 algorithm class.
-		*/
-		class CRC8
-		{
-		public:
-			/*!
-			 	@brief Constructor.
-			*/
-			GINL CRC8(){m_crc = 0; this->prepare();}
-			
-			/*!
-			 	@brief Clear current CRC context.
-			*/
-			GINL GAIA::GVOID clear(){m_crc = 0;}
-			
-			/*!
-			 	@brief Get CRC result.
-			 
-			 	@return Return CRC8 result.
-			*/
-			GINL GAIA::U8 result() const{return m_crc;}
-			
-			/*!
-			 	@brief Write more binary data for calculating CRC result.
-			 
-			 	@param pBuf [in] Specify the buffer which will be writen.
-			 
-			 	@param size [in] Specify the size of parameter pBuf in bytes.
-			 
-			 	@return If success, return GAIA::True, or will return GAIA::False.
-			*/
-			GINL GAIA::BL update(const GAIA::GVOID* pBuf, GAIA::U32 size)
-			{
-				GAST(!!pBuf);
-				GAST(size > 0);
-				if(pBuf == GNIL)
-					return GAIA::False;
-				if(size <= 0)
-					return GAIA::False;
-				return GAIA::True;
-			}
-		private:
-			GINL GAIA::GVOID prepare(){}
-		private:
-			GAIA::U8 m_crc;
-			GAIA::CTN::Vector<GAIA::U8> m_crctable;
-		};
-		
-		/*!
-		 	@brief Stream based CRC16 algorithm class.
-		*/
-		class CRC16
-		{
-		public:
-			/*!
-			 	@brief Constructor.
-			*/
-			GINL CRC16(){m_crc = 0; this->prepare();}
-			
-			/*!
-			 	@brief Clear current CRC context.
-			*/
-			GINL GAIA::GVOID clear(){m_crc = 0;}
-			
-			/*!
-			 	@brief Get CRC result.
-			 
-			 	@return Return CRC16 result.
-			*/
-			GINL GAIA::U16 result() const{return m_crc;}
-			
-			/*!
-			 	@brief Write more binary data for calculating CRC result.
-			 
-			 	@param pBuf [in] Specify the buffer which will be writen.
-			 
-			 	@param size [in] Specify the size of parameter pBuf in bytes.
-			 
-			 	@return If success, return GAIA::True, or will return GAIA::False.
-			*/
-			GINL GAIA::BL update(const GAIA::GVOID* pBuf, GAIA::U32 size)
-			{
-				GAST(!!pBuf);
-				GAST(size > 0);
-				if(pBuf == GNIL)
-					return GAIA::False;
-				if(size <= 0)
-					return GAIA::False;
-				return GAIA::True;
-			}
-		private:
-			GINL GAIA::GVOID prepare(){}
-		private:
-			GAIA::U16 m_crc;
-			GAIA::CTN::Vector<GAIA::U16> m_crctable;
-		};
+		extern const GAIA::U32 CRC_MAP32[256];
 		
 		/*!
 		 	@brief Stream based CRC32 algorithm class.
@@ -116,12 +20,19 @@ namespace GAIA
 			/*!
 			 	@brief Constructor.
 			*/
-			GINL CRC32(){m_crc = 0; this->prepare();}
+			GINL CRC32(){m_crc = 0;}
 			
 			/*!
-			 	@brief Clear current CRC context.
+			 	@brief Check current CRC context is changed or not.
+				 
+				@return If changed, return GAIA::True, or will return GAIA::False.
 			*/
-			GINL GAIA::GVOID clear(){m_crc = 0;}
+			GINL GAIA::BL check() const{return m_crc != 0;}
+			
+			/*!
+			 	@brief Reset current CRC context to default.
+			*/
+			GINL GAIA::GVOID reset(){m_crc = 0;}
 			
 			/*!
 			 	@brief Get CRC result.
@@ -151,43 +62,39 @@ namespace GAIA
 				m_crc = m_crc ^ 0xFFFFFFFF;
 				while (size >= 8)
 				{
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
 					size -= 8;
 				}
 				while(size > 0)
 				{
-					m_crc = m_crctable[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
+					m_crc = CRC_MAP32[((GAIA::N32)m_crc ^ (*p++)) & 0xFF] ^ (m_crc >> 8);
 					--size;
 				}
 				m_crc ^= 0xFFFFFFFF;
 				return GAIA::True;
 			}
-		private:
-			GINL GAIA::GVOID prepare()
+			
+			/*!
+			 	@brief Write a data type for calculate CRC result.
+			 
+			 	@param t [in] Specify a data type.
+			 
+			 	@return If success, return GAIA::True, or will return GAIA::False.
+			*/
+			template<typename _ParamDataType> GAIA::BL update(const _ParamDataType& t)
 			{
-				static const GAIA::U8 p[] = {0, 1, 2, 4, 5, 7, 8, 10, 11, 12, 16, 22, 23, 26};
-				m_crctable.resize(256);
-				GAIA::U32 poly = 0;
-				for(GAIA::N32 x = 0; x < sizeofarray(p); x++)
-					poly |= 1 << (31 - p[x]);
-				for(GAIA::N32 x = 0; x < 256; x++)
-				{
-					GAIA::U32 c = GSCAST(GAIA::U32)(x);
-					for(GAIA::N32 y = 0; y < 8; y++)
-						c = c & 1 ? poly ^ (c >> 1) : c >> 1;
-					m_crctable[x] = c;
-				}
+				return this->update(&t, sizeof(t));
 			}
+			
 		private:
 			GAIA::U32 m_crc;
-			GAIA::CTN::Vector<GAIA::U32> m_crctable;
 		};
 	}
 }
