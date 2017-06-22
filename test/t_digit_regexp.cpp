2cebd94280dfa0286b27c9ad785bbfaa1bc353d9
Armterla <70601797@qq.com>
2017-06-22 21:25:18 +0800

    Test case : RegExp test case impl go a step further.

--- test/t_digit_regexp.cpp
+++ test/t_digit_regexp.cpp
@@ -17,60 +17,149 @@ namespace TEST
 		re.SetResultMaxLen(10);
 		const GAIA::TCH* pszSource = _T("#include <stdlib.h>\r\nint main(){\r\n\treturn 0;\r\n}");
 
-		const GAIA::TCH* pszFinder = _T("#include");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		const __RegExpType::ResultInfo& ri = re.GetResultInfo();
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("#include");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 0)
+					TERROR;
+				if(ri.offsets[0].nEnd != 8)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("#include");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("#include");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 0)
+					TERROR;
+				if(ri.offsets[0].nEnd != 8)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("stdlib.h");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("stdlib.h");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 10)
+					TERROR;
+				if(ri.offsets[0].nEnd != 18)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("<stdlib.h>");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("<stdlib.h>");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 9)
+					TERROR;
+				if(ri.offsets[0].nEnd != 19)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("int main");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("int main");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 21)
+					TERROR;
+				if(ri.offsets[0].nEnd != 29)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("int main\\(\\)");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("int main\\(\\)");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 21)
+					TERROR;
+				if(ri.offsets[0].nEnd != 31)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("^");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("^");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 0)
+					TERROR;
+				if(ri.offsets[0].nEnd != 0)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("\t");
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("\t");
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 34)
+					TERROR;
+				if(ri.offsets[0].nEnd != 35)
+					TERROR;
+			}
+		}
 
-		pszFinder = _T("^int\\smain.*$");
-		co.bMultiLine = true;
-		TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
-		TAST(re.Execute(pszSource, -1, 0, eo));
-		if(ri.offsets.size() != 1)
-			TERROR;
+		{
+			const GAIA::TCH* pszFinder = _T("^int\\smain.*$");
+			co.bMultiLine = true;
+			TAST(re.Compile(pszFinder, co, sizeof(GAIA::TCH)));
+			TAST(re.Execute(pszSource, -1, 0, eo));
+			const __RegExpType::ResultInfo& ri = re.GetResultInfo();
+			if(ri.offsets.size() != 1)
+				TERROR;
+			else
+			{
+				if(ri.offsets[0].nBegin != 21)
+					TERROR;
+				if(ri.offsets[0].nEnd != 33)
+					TERROR;
+			}
+		}
 	}
 }
