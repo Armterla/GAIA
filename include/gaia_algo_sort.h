d61439651858cfa1a927ae15ec1d20d8fa244f59
Armterla <70601797@qq.com>
2017-07-14 14:17:32 +0800

    New feature : Sort algorithm support comparator function. Test case : Add some test case for the new features in current commit.

--- include/gaia_algo_sort.h
+++ include/gaia_algo_sort.h
@@ -31,49 +31,21 @@ namespace GAIA
 		}
 		template<typename _DataType> GAIA::GVOID isort(_DataType pBegin, _DataType pEnd) // Insertion sort.
 		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
 		}
 		template<typename _DataType> GAIA::GVOID ssort(_DataType pBegin, _DataType pEnd) // Selection sort.
 		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
 		}
 		template<typename _DataType> GAIA::GVOID hsort(_DataType pBegin, _DataType pEnd) // Heap sort.
 		{
-		}
-		template<typename _DataType> GAIA::BL ksort(_DataType pBegin, _DataType pEnd) // Bucket sort.
-		{
 			GAST(!!pBegin);
 			GAST(!!pEnd);
-			if(!isintegertype(*pBegin))
-				return GAIA::False;
-			static const GAIA::NUM ELEMENT_COUNT = 0x8000;
-			GAIA::N64 nMax = GAIA::NMMIN;
-			GAIA::N64 nMin = GAIA::NMMAX;
-			_DataType pTemp = pBegin;
-			while(pTemp <= pEnd)
-			{
-				if(*pTemp > nMax)
-					nMax = *pTemp;
-				if(*pTemp < nMin)
-					nMin = *pTemp;
-				++pTemp;
-			}
-			if(nMax < nMin)
-				return GAIA::False;
-			if(nMax - nMin >= ELEMENT_COUNT)
-				return GAIA::False;
-			GAIA::NUM lists[ELEMENT_COUNT];
-			for(GAIA::NUM x = 0; x < ELEMENT_COUNT; ++x)
-				lists[x] = 0;
-			pTemp = pBegin;
-			while(pTemp <= pEnd)
-			{
-				++lists[*pTemp - nMin];
-				++pTemp;
-			}
-			pTemp = pBegin;
-			for(GAIA::NUM x = 0; x < ELEMENT_COUNT; ++x)
-				for(GAIA::NUM y = 0; y < lists[x]; ++y)
-					*pTemp++ = x + nMin;
-			return GAIA::True;
+			GAST(pBegin < pEnd);
 		}
 		template<typename _DataType> GAIA::GVOID esort(_DataType pBegin, _DataType pEnd) // External sort.
 		{
@@ -92,7 +64,7 @@ namespace GAIA
 		#else
 			// quick sort optimize.
 			if(pEnd - pBegin < 16)
-				return bsort(pBegin, pEnd);
+				return GAIA::ALGO::bsort(pBegin, pEnd);
 			GAIA::ALGO::swap(*pEnd, *(pBegin + (pEnd - pBegin) / 2));
 
 			/* Standard quick sort algorithm. */
@@ -132,6 +104,151 @@ namespace GAIA
 					return GAIA::False;
 				++pBegin;
 			}
+			return GAIA::True;
+		}
+		template<typename _DataType> GAIA::GVOID bsort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Bubble sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			while(pBegin < pEnd)
+			{
+				_DataType pTemp = pBegin + 1;
+				while(pTemp <= pEnd)
+				{
+					if(cmpor(pTemp, pBegin) < 0)
+						GAIA::ALGO::swap(*pBegin, *pTemp);
+					++pTemp;
+				}
+				++pBegin;
+			}
+		}
+		template<typename _DataType> GAIA::GVOID isort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Insertion sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
+		}
+		template<typename _DataType> GAIA::GVOID ssort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Selection sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
+		}
+		template<typename _DataType> GAIA::GVOID hsort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Heap sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
+		}
+		template<typename _DataType> GAIA::GVOID esort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // External sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
+		}
+		template<typename _DataType> class ComparatorForSTL : public std::binary_function<_DataType, _DataType, GAIA::BL>
+		{
+		public:
+			GINL ComparatorForSTL(GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2))
+			{
+				this->cmpor = cmpor;
+			}
+			GINL GAIA::BL operator()(const _DataType& a, const _DataType& b)  
+			{
+				return cmpor(&a, &b) < 0;
+			}
+		private:
+			GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2);
+		};
+		template<typename _DataType> GAIA::GVOID qsort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Quick sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			if(pBegin >= pEnd)
+				return;
+		#ifdef GAIA_USESTL
+			ComparatorForSTL<decltype(*pBegin)> stlcmpor(cmpor);
+			std::sort(pBegin, pEnd + 1, stlcmpor);
+		#else
+			// quick sort optimize.
+			if(pEnd - pBegin < 16)
+				return GAIA::ALGO::bsort(pBegin, pEnd, cmpor);
+			GAIA::ALGO::swap(*pEnd, *(pBegin + (pEnd - pBegin) / 2));
+
+			/* Standard quick sort algorithm. */
+			_DataType pCursor = pBegin;
+			_DataType pSplit = pBegin;
+			while(pCursor < pEnd)
+			{
+				if(cmpor(pCursor, *pEnd) < 0) // pEnd is the key.
+				{
+					GAIA::ALGO::swap(*pCursor, *pSplit);
+					++pSplit;
+				}
+				++pCursor;
+			}
+			GAIA::ALGO::swap(*pSplit, *pEnd);
+			GAIA::ALGO::qsort(pBegin, pSplit - 1, cmpor);
+			GAIA::ALGO::qsort(pSplit + 1, pEnd, cmpor);
+		#endif
+		}
+		template<typename _DataType> GAIA::GVOID sort(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2)) // Sort wrapper.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			if(pEnd - pBegin < 10)
+				GAIA::ALGO::bsort(pBegin, pEnd, cmpor);
+			else
+				GAIA::ALGO::qsort(pBegin, pEnd, cmpor);
+		}
+		template<typename _DataType> GAIA::BL issorted(_DataType pBegin, _DataType pEnd, GAIA::N32 (*cmpor)(const GAIA::GVOID* t1, const GAIA::GVOID* t2))
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			GAST(pBegin < pEnd);
+			while(pBegin != pEnd)
+			{
+				if(cmpor(pBegin, pBegin + 1) > 0)
+					return GAIA::False;
+				++pBegin;
+			}
+			return GAIA::True;
+		}
+		template<typename _DataType> GAIA::BL ksort(_DataType pBegin, _DataType pEnd) // Bucket sort.
+		{
+			GAST(!!pBegin);
+			GAST(!!pEnd);
+			if(!isintegertype(*pBegin))
+				return GAIA::False;
+			static const GAIA::NUM ELEMENT_COUNT = 0x8000;
+			GAIA::N64 nMax = GAIA::NMMIN;
+			GAIA::N64 nMin = GAIA::NMMAX;
+			_DataType pTemp = pBegin;
+			while(pTemp <= pEnd)
+			{
+				if(*pTemp > nMax)
+					nMax = *pTemp;
+				if(*pTemp < nMin)
+					nMin = *pTemp;
+				++pTemp;
+			}
+			if(nMax < nMin)
+				return GAIA::False;
+			if(nMax - nMin >= ELEMENT_COUNT)
+				return GAIA::False;
+			GAIA::NUM lists[ELEMENT_COUNT];
+			for(GAIA::NUM x = 0; x < ELEMENT_COUNT; ++x)
+				lists[x] = 0;
+			pTemp = pBegin;
+			while(pTemp <= pEnd)
+			{
+				++lists[*pTemp - nMin];
+				++pTemp;
+			}
+			pTemp = pBegin;
+			for(GAIA::NUM x = 0; x < ELEMENT_COUNT; ++x)
+				for(GAIA::NUM y = 0; y < lists[x]; ++y)
+					*pTemp++ = x + nMin;
 			return GAIA::True;
 		}
 	}
--- test/t_algo_sort.cpp
+++ test/t_algo_sort.cpp
@@ -3,37 +3,105 @@
 
 namespace TEST
 {
+	class BaseBase : public virtual GAIA::Base
+	{
+	public:
+		virtual GAIA::N32 func(){return m_nbase;}
+	public:
+		GAIA::N32 m_nbase;
+	};
+	class Base1 : public virtual BaseBase
+	{
+	public:
+		virtual GAIA::N32 func() const{return m_n1;}
+	public:
+		GAIA::N32 m_n1;
+	};
+	class Base2 : public virtual BaseBase
+	{
+	public:
+		virtual GAIA::N32 func() const{return m_n2;}
+	public:
+		GAIA::N32 m_n2;
+	};
+	class Node : public virtual Base1, public virtual Base2
+	{
+	public:
+		virtual GAIA::N32 func() const{return m_v;}
+	public:
+		GAIA::NUM m_v;
+	};
+	GINL GAIA::N32 CmporForNode(const GAIA::GVOID* p1, const GAIA::GVOID* p2)
+	{
+		const Node& t1 = *(Node*)p1;
+		const Node& t2 = *(Node*)p2;
+		if(t1.m_v < t2.m_v)
+			return -1;
+		else if(t1.m_v > t2.m_v)
+			return +1;
+		return 0;
+	}
 	extern GAIA::GVOID t_algo_sort(GAIA::LOG::Log& logobj)
 	{
-		// Generate test data.
-		GAIA::MATH::RandomLCG lcg;
-		GAIA::CTN::Vector<GAIA::N64> listData;
-		listData.resize(1000);
-		for(GAIA::NUM x = 0; x < listData.size(); ++x)
-			listData[x] = lcg.random_u16() % 0x8000 - 2000;
-		GAIA::CTN::Vector<GAIA::N64> listDataSorted = listData;
-		GAIA::ALGO::sort(listDataSorted.fptr(), listDataSorted.bptr());
-
-		// Bucket sort.
-		GAIA::CTN::Vector<GAIA::N64> listDataOrigin = listData;
-		if(!GAIA::ALGO::ksort(listData.fptr(), listData.bptr()))
-			TERROR;
-		if(!GAIA::ALGO::issorted(listData.fptr(), listData.bptr()))
-			TERROR;
-		for(GAIA::NUM x = 0; x < listData.size(); ++x)
+		// Without comparator test.
 		{
-			if(listData[x] != listDataSorted[x])
+			// Generate test data.
+			GAIA::MATH::RandomLCG lcg;
+			GAIA::CTN::Vector<GAIA::N64> listData;
+			listData.resize(1000);
+			for(GAIA::NUM x = 0; x < listData.size(); ++x)
+				listData[x] = lcg.random_u16() % 0x8000 - 2000;
+			GAIA::CTN::Vector<GAIA::N64> listDataSorted = listData;
+			GAIA::ALGO::sort(listDataSorted.fptr(), listDataSorted.bptr());
+
+			// Bucket sort.
+			GAIA::CTN::Vector<GAIA::N64> listDataOrigin = listData;
+			if(!GAIA::ALGO::ksort(listData.fptr(), listData.bptr()))
 				TERROR;
-		}
+			if(!GAIA::ALGO::issorted(listData.fptr(), listData.bptr()))
+				TERROR;
+			for(GAIA::NUM x = 0; x < listData.size(); ++x)
+			{
+				if(listData[x] != listDataSorted[x])
+					TERROR;
+			}
 
-		// Quick sort.
-		listData = listDataOrigin;
-		GAIA::ALGO::qsort(listData.fptr(), listData.bptr());
-		if(!GAIA::ALGO::issorted(listData.fptr(), listData.bptr()))
-			TERROR;
-		for(GAIA::NUM x = 0; x < listData.size(); ++x)
+			// Quick sort.
+			listData = listDataOrigin;
+			GAIA::ALGO::qsort(listData.fptr(), listData.bptr());
+			if(!GAIA::ALGO::issorted(listData.fptr(), listData.bptr()))
+				TERROR;
+			for(GAIA::NUM x = 0; x < listData.size(); ++x)
+			{
+				if(listData[x] != listDataSorted[x])
+					TERROR;
+			}
+		}
+		
+		// With comparator test.
 		{
-			if(listData[x] != listDataSorted[x])
+			GAIA::CTN::Vector<Node> listNode;
+			listNode.resize(1000);
+			for(GAIA::NUM x = 0; x < listNode.size(); ++x)
+				listNode[x].m_v = x;
+			GAIA::MATH::RandomLCG lcg;
+			for(GAIA::NUM x = 0; x < listNode.size(); ++x)
+			{
+				GAIA::NUM a = lcg.random() % listNode.size();
+				GAIA::NUM b = lcg.random() % listNode.size();
+				if(a != b)
+					listNode.swap(a, b);
+			}
+			GAIA::ALGO::sort(listNode.fptr(), listNode.bptr(), CmporForNode);
+			for(GAIA::NUM x = 0; x < listNode.size(); ++x)
+			{
+				if(listNode[x].m_v != x)
+				{
+					TERROR;
+					break;
+				}
+			}
+			if(!GAIA::ALGO::issorted(listNode.fptr(), listNode.bptr(), CmporForNode))
 				TERROR;
 		}
 	}
