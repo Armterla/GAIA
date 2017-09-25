d1b1dbc167b91e28bbfc91489e0d463f830b21f8
Armterla <70601797@qq.com>
2017-09-25 11:57:34 +0800

    goon

--- test/t_digit_hmac.cpp
+++ test/t_digit_hmac.cpp
@@ -5,8 +5,10 @@ namespace TEST
 {
 	extern GAIA::GVOID t_digit_hmac(GAIA::LOG::Log& logobj)
 	{
+		static const GAIA::NUM DATA_SIZE = 10000;
+		static const GAIA::NUM KEY_SIZE = 50;
 		GAIA::CTN::AString strData;
-		for(GAIA::NUM x = 0; x < 10; ++x)
+		for(GAIA::NUM x = 0; x < DATA_SIZE; ++x)
 		{
 			GAIA::CH szTemp[2];
 			szTemp[0] = '0' + (x % 10);
@@ -15,7 +17,7 @@ namespace TEST
 		}
 		
 		GAIA::CTN::AString strKey;
-		for(GAIA::NUM x = 0; x < 10; ++x)
+		for(GAIA::NUM x = 0; x < KEY_SIZE; ++x)
 		{
 			GAIA::CH szTemp[2];
 			szTemp[0] = '0' + (x % 10);
@@ -24,13 +26,13 @@ namespace TEST
 		}
 		
 		GAIA::U8 res[20];
-		GAIA::DIGIT::hmacsha1(strKey.fptr(), strKey.size(), strKey.fptr(), strKey.size(), res);
+		GAIA::DIGIT::hmacsha1(strData.fptr(), strData.size(), strKey.fptr(), strKey.size(), res);
 		GAIA::ALGO::inverse(res, res + sizeofarray(res) - 1);
 		
 		GAIA::CH szTemp[41];
 		GAIA::ALGO::hex2str(res, sizeofarray(res), szTemp);
 		
-		if(!GAIA::ALGO::gstriequal(szTemp, "cb070897dd78957c2a2976105b4b38ede43cb3e4"))
+		if(!GAIA::ALGO::gstriequal(szTemp, "91d0deaff2c413a3aea50a872f3d0d538ab5b6fe"))
 			TERROR;
 	}
 }
